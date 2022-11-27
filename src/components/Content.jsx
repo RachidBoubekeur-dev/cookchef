c1f43bcafac44a16647faacfec37ca2dbcc6e11d
Rachid Boubekeur <rachidboubekeur@free.fr>
2022-11-27 09:20:44 +0100

    :seedling: Setting up content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,24 @@
+import styles from './Content.module.scss';
+
 const Content = () => {
     return (
-        <div className='flex-fill b2'>
-            <h1>Content</h1>
+        <div className='flex-fill container p-20'>
+            <h1 className='my-30'>Découvrez nos nouvelles recettes</h1>
+            <div className={`card p-20 ${styles.contentCard}`}>
+                <div className={styles.grid}>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                    <div className={styles.elem}>Element</div>
+                </div>
+            </div>
         </div>
     );
 };
