ff3fa2517d9ac5b4bb4ecfba8af6d7476f4376b3
Rachid Boubekeur <rachidboubekeur@free.fr>
2022-12-01 06:31:29 +0100

    :construction: add a search bar

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import styles from './Content.module.scss';
 import Recipe from './Recipe';
 import { data } from '../data/recipes';
 
 const Content = () => {
     const recipes = data;
+    const [filter, setFilter] = useState('');
+
     return (
         <div className='flex-fill container p-20'>
             <h1 className='my-30'>Découvrez nos nouvelles recettes</h1>
             <div className={`card p-20 ${styles.contentCard}`}>
+                <div className={`d-flex mb-20 pl-15 ${styles.searchBar}`}>
+                    <label for='searchBar'><i className='fa-solid fa-magnifying-glass mr-15'></i></label>
+                    <input
+                        onInput={(e) => setFilter(e.target.value.trim().toLowerCase())}
+                        type='text' id='searchBar' placeholder='Rechercher une recette' />
+                </div>
                 <div className={styles.grid}>
-                    {recipes.map((recipe, i) => (
-                        <Recipe key={i} recipe={recipe} />
+                    {recipes.filter((recipe) => recipe.title.toLowerCase().startsWith(filter)).map((recipe) => (
+                        <Recipe key={recipe._id} recipe={recipe} />
                     ))}
                 </div>
             </div>
